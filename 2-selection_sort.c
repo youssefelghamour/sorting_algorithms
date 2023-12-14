261a6a0698d3ccfb9c758d8b5928724356e49c6a
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-14 00:57:20 -0800

    2. Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int low, min_idx, temp;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		low = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < low)
+			{
+				low = array[j];
+				min_idx = j;
+			}
+		}
+		if (low != array[i])
+		{
+			temp = array[i];
+			array[i] = low;
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
