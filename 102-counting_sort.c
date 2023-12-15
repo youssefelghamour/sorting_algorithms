186f55d6db4b8f2ffbdcb6aa0107272f77327d75
youssef <youssefelghamour@gmail.com>
2023-12-15 13:08:12 -0800

    add 102-counting_sort.c

---
+++ 102-counting_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending order using the
+ *		   Counting sort algorithm
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *index, *places;
+	int i;
+
+	places = malloc(sizeof(int) * size);
+	if (!places)
+		return;
+
+	int max = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] >= max)
+			max = array[i];
+	}
+
+	index = malloc(sizeof(int) * (max + 1));
+	if (!index)
+		return;
+
+	for (i = 0; i < size; i++)
+		index[array[i]]++;
+
+	for (i = 0; i <= max; i++)
+		index[i] = index[i] + index[i - 1];
+
+	print_array(index, max + 1);
+
+	for (i = 0; i < size; i++)
+	{
+		places[index[array[i]] - 1] = array[i];
+		index[array[i]]--;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = places[i];
+
+	free(index);
+	free(places);
+}
--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif
