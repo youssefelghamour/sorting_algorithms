0d348bc4c5e600a5048b5f8c561d6980d8bbfadd
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-14 17:07:05 -0800

    3. Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap -  swap two elements of the array
+ * @array: array to swap its elements
+ * @a: first element
+ * @b: second element
+ * Return: Nothing
+ */
+void swap(int *array, int a, int b)
+{
+	int temp;
+
+	temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
+/**
  * lomuto - uses lomuto partition scheme to divide the array
  * @array: array to be sorted
  * @low: lower bound of the array
@@ -9,21 +24,17 @@
  */
 int lomuto(int *array, int low, int up)
 {
-	int pivot = array[up], i, j = low, temp;
+	int pivot = array[up], i, j = low;
 
 	for (i = low; i < up; i++)
 	{
 		if (array[i] <= pivot)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			swap(array, i, j);
 			j++;
 		}
 	}
-	temp = array[up];
-	array[up] = array[j];
-	array[j] = temp;
+	swap(array, up, j);
 	return (j);
 }
 
@@ -32,18 +43,19 @@ int lomuto(int *array, int low, int up)
  * @array: array to be sorted
  * @low: lower bound of the array
  * @up: upper bound of the array
+ * @size: size of the array
  * Return: Nothing
  */
-void sorting(int *array, int low, int up)
+void sorting(int *array, int low, int up, size_t size)
 {
 	int p;
 
 	if (low >= up || low < 0)
 		return;
 	p = lomuto(array, low, up);
-	sorting(array, low, p - 1);
-	sorting(array, p + 1, up);
-	print_array(array, up + 1);
+	sorting(array, low, p - 1, size);
+	sorting(array, p + 1, up, size);
+	print_array(array, size);
 }
 
 /**
@@ -56,5 +68,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (size < 2)
 		return;
-	sorting(array, 0, size - 1);
+	sorting(array, 0, size - 1, size);
 }
