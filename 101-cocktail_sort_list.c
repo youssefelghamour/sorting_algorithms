97969ba72cee32ef46493e0802d3f03ccfc66eb2
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-16 07:37:47 -0800

    5. Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -11,12 +11,8 @@ void swap(listint_t *x, listint_t *temp, listint_t **list)
 {
 	if (temp->next != NULL)
 		temp->next->prev = x;
-	else
-		*list = temp;
 	if (x->prev != NULL)
 		x->prev->next = temp;
-	else
-		*list = temp;
 	x->next = temp->next;
 	temp->prev = x->prev;
 	x->prev = temp;
