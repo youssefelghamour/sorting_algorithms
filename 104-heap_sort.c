e5bb0122c11b18ddfcf011eea7d2872dfc60987e
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-16 23:19:39 -0800

    8. Heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -61,7 +61,8 @@ void heap_sort(int *array, size_t size)
 	for (i = size - 1; i >= 0; i--)
 	{
 		swap(array, 0, i);
-		print_array(array, size);
+		if (i != 0)
+			print_array(array, size);
 		heapify(array, i, 0, size);
 	}
 }
