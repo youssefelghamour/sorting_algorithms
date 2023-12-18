dc60e91ac961c502a2c2009655395c3248e1d4f5
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-18 03:50:05 -0800

    11. Quick Sort - Hoare Partition scheme

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -16,30 +16,48 @@ void swap(int *array, int x, int y)
 	array[y] = temp;
 }
 
+/**
+ * hoare - move all elements smaller than pivot left and higher numbers right
+ * @array: array to be sorted
+ * @i: lowest index
+ * @j: max index
+ * @size: size of the array
+ * Return: Nothing
+ */
 int hoare(int *array, int i, int j, size_t size)
 {
 	int pivot = array[j];
-	int low = i, high = j;
+	int low = i - 1, high = j + 1;
 
 	while (low < high)
 	{
-		while (array[low] < pivot)
+		do {
 			low++;
-		while (array[high] > pivot)
+		} while (array[low] < pivot);
+		do {
 			high--;
+		} while (array[high] > pivot);
 		if (low < high)
 		{
 			swap(array, low, high);
 			print_array(array, size);
 		}
 	}
-	return (high);
+	return (low);
 }
 
+/**
+ * quick_sort2 - partition the array
+ * @array: array to be sorted
+ * @i: lowest index
+ * @j: max index
+ * @size: size of the array
+ * Return: Nothing
+ */
 void quick_sort2(int *array, int i, int j, size_t size)
 {
 	int p;
-	
+
 	if (i >= j)
 		return;
 	p = hoare(array, i, j, size);
@@ -47,6 +65,12 @@ void quick_sort2(int *array, int i, int j, size_t size)
 	quick_sort2(array, p, j, size);
 }
 
+/**
+ * quick_sort_hoare - sorts array of integers using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: Nothing
+ */
 void quick_sort_hoare(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
