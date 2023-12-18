8af49890515271ed6c9a19ea1365e71d350b0916
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-17 21:31:10 -0800

    10. Bitonic sort

--- sort.h
+++ sort.h
@@ -20,7 +20,6 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int *array, int x, int y);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
